23037ff2497517de23609d500a000b5894690212
Julio Del Valle <jdelvallegam@microsoft.com>
2024-04-17 10:16:17 -0600

    Finishing code structuring lesson

--- 26_codeStructuringForBiggerProjects/src/Experience/Experience.js
+++ 26_codeStructuringForBiggerProjects/src/Experience/Experience.js
@@ -5,6 +5,7 @@ import Renderer from './Renderer'
 import World from './World/World'
 import Resources from './Utils/Resources'
 import sources from './sources.js'
+import Debug from './Utils/Debug.js'
 
 import * as THREE from 'three'
 
@@ -27,6 +28,7 @@ export default class Experience {
         this.canvas = canvas
 
         // Setup
+        this.debug = new Debug()
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
@@ -44,7 +46,6 @@ export default class Experience {
         this.sizes.on('resize', () =>
         {
             this.resize()
-            console.log('A resize occurred')
         })
     }
 
@@ -57,5 +58,39 @@ export default class Experience {
         this.camera.update()
         this.world.update()
         this.renderer.update()
+    }
+
+    destroy() {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        // Traverse the whole scene
+        this.scene.traverse((child) =>
+        {
+            // Test if it's a mesh
+            if(child instanceof THREE.Mesh)
+            {
+                child.geometry.dispose()
+
+                // Loop through the material properties
+                for(const key in child.material)
+                {
+                    const value = child.material[key]
+
+                    // Test if there is a dispose function
+                    if(value && typeof value.dispose === 'function')
+                    {
+                        value.dispose()
+                    }
+                }
+            }
+
+            this.camera.controls.dispose()
+            this.renderer.instance.dispose()
+
+            if(this.debug.active){
+                this.debug.ui.destroy()
+            }
+        })
     }
 }
