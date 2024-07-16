d2481db5cd6638203882a6a9f2c1a91a2b077d6d
Julio Del Valle <jdelvallegam@microsoft.com>
2024-07-15 20:40:27 -0600

    Adding 63 and 64 projects

---
+++ 63_postProcessingWithr3f/src/DrunkEffect.jsx
@@ -0,0 +1,44 @@
+import { BlendFunction, Effect } from 'postprocessing'
+import { Uniform } from 'three'
+
+const fragmentShader = /* glsl */`
+    uniform float frequency;
+    uniform float amplitude;
+    uniform float time;
+
+    void mainUv(inout vec2 uv)
+    {
+        uv.y += sin(uv.x * frequency + time) * amplitude;
+    }
+
+    void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
+    {
+        outputColor = vec4(0.8, 1.0, 0.5, inputColor.a);
+    }
+`
+
+export default class DrunkEffect extends Effect
+{
+    constructor({ frequency, amplitude, blendFunction = BlendFunction.DARKEN })
+    {
+        console.log(frequency, amplitude)
+        
+        super(
+            'DrunkEffect',
+            fragmentShader,
+            {
+                uniforms: new Map([
+                    [ 'frequency', new Uniform(frequency) ],
+                    [ 'amplitude', new Uniform(amplitude) ],
+                    [ 'time', new Uniform(0) ]
+                ]),
+                blendFunction
+            },
+        )
+    }
+
+    update(renderer, inputBuffer, deltaTime)
+    {
+        this.uniforms.get('time').value += deltaTime
+    }
+}
--- 63_postProcessingWithr3f/src/Experience.jsx
+++ 63_postProcessingWithr3f/src/Experience.jsx
@@ -1,9 +1,51 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { DepthOfField, Bloom, Noise, Glitch, ToneMapping, Vignette, EffectComposer } from '@react-three/postprocessing'
+import {  GlitchMode, BlendFunction, ToneMappingMode } from 'postprocessing'
+import Drunk from './Drunk'
+import { useRef } from 'react'
+import { useControls } from 'leva'
 
 export default function Experience()
 {
+    const drunkRef = useRef()
+
+    const drunkProps = useControls('Drunk Effect', {
+        frequency: { value: 2, min: 1, max: 20 },
+        amplitude: { value: 0.1, min: 0, max: 1 }
+    })
+
     return <>
+        <color args={ [ '#FFFFFF' ] } attach="background" />
+        <EffectComposer>
+            <ToneMapping mode={ ToneMappingMode.ACES_FILMIC } />
+            {/*<Vignette
+                offset={ 0.3 }
+                darkness={ 0.9 }
+                blendFunction={ BlendFunction.NORMAL }
+            />
+            <Glitch
+                delay={ [ 0.5, 1 ] }
+                duration={ [ 0.1, 0.3 ] }
+                strength={ [ 0.2, 0.4 ] }
+                mode={ GlitchMode.CONSTANT_MILD }
+            />
+            <Noise
+                premultiply
+                blendFunction={ BlendFunction.SOFT_LIGHT }
+            /> */}
+            {/* <Bloom
+                mipmapBlur
+                intensity={ 0.5 }
+                luminanceThreshold={ 0 }
+            /> */}
+            <Drunk
+                ref={ drunkRef }
+                { ...drunkProps }
+                blendFunction={ BlendFunction.DARKEN }
+            />
+        </EffectComposer>
+
 
         <Perf position="top-left" />
 
@@ -17,6 +59,10 @@ export default function Experience()
             <meshStandardMaterial color="orange" />
         </mesh>
 
+        {/* <mesh castShadow position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="orange" emissive="orange" emissiveIntensity={ 2 } />
+        </mesh> */}
         <mesh castShadow position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
