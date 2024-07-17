ae8305e8ef92726fe30d7aa1f2790e08e0c47e2c
Julio Del Valle <jdelvallegam@microsoft.com>
2024-07-17 16:39:15 -0600

    adding lesson 65

--- 65_physics/src/index.jsx
+++ 65_physics/src/index.jsx
@@ -6,12 +6,13 @@ import Experience from './Experience.jsx'
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas className='r3f'
+    <Canvas
+        shadows
         camera={ {
             fov: 45,
             near: 0.1,
-            far: 2000,
-            position: [ -3, 1.5, 4 ]
+            far: 200,
+            position: [ 4, 2, 6 ]
         } }
     >
         <Experience />
