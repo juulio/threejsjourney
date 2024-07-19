d8b80343a79bc80bf30ba87f96eed84c2a4e84e9
Julio Del Valle <jdelvallegam@microsoft.com>
2024-07-19 11:54:38 -0600

    adding lesson 66 code

---
+++ 66_createAGameWithR3F/src/Level.jsx
@@ -0,0 +1,31 @@
+function BlockStart() {
+  return null;
+}
+
+export default function Level() {
+  return (
+    <>
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+
+      <BlockStart></BlockStart>
+    </>
+  );
+}
